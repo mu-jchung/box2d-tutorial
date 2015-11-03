aef8490c9ad595649fd9abc760fca53ecf87a978
mu-jchung <chungjodad@gmail.com>
2015-11-03 15:53:11 -0500

    Added last part (Physica 5 - filters)

--- desktop/src/com/gamefromscratch/desktop/DesktopLauncher.java
+++ desktop/src/com/gamefromscratch/desktop/DesktopLauncher.java
@@ -6,11 +6,12 @@ import com.gamefromscratch.Physics1;
 import com.gamefromscratch.Physics2;
 import com.gamefromscratch.Physics3;
 import com.gamefromscratch.Physics4;
+import com.gamefromscratch.Physics5;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		// new LwjglApplication(new Physics1(), config);
-		new LwjglApplication(new Physics3(), config);
+		new LwjglApplication(new Physics5(), config);
 	}
 }
